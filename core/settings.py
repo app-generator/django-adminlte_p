c6ad7fd8b4eb818496e2b8ff30d54bf6cdd36f5a
App Generator <51070104+app-generator@users.noreply.github.com>
2022-12-08 16:34:20 +0200

    Dummy

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'admin_material.apps.AdminMaterialDashboardConfig',
+    'admin_adminlte.apps.AdminAdminlteConfig',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- core/urls.py
+++ core/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('app1.urls')),
     path("admin/", admin.site.urls),
-    path("", include('admin_material.urls'))
+    path("", include('admin_adminlte.urls'))
 ]
